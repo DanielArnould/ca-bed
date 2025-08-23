4a98c6e19a8cb6148fa9dc0ae167cccdabf55f06
DanielArnould <darn0005@student.monash.edu>
2025-08-23 22:18:11 +1000

    Rename node fields to match formal method

--- node.py
+++ node.py
@@ -11,30 +11,30 @@ type Item = str
 
 
 @dataclass
-class AnswerNode:
+class EvidenceNode:
     answer: str
-    item_probabilities: list[tuple[Item, Probability]]
-    this_group: "QuestionGroup | None" = None
-    children_groups: list["QuestionGroup"] = field(default_factory=list)
+    belief_state: list[tuple[Item, Probability]]
+    parent: "QuestionNode | None" = None
+    children: list["QuestionNode"] = field(default_factory=list)
 
 
 @dataclass
-class QuestionGroup:
+class QuestionNode:
     question: str
-    parent: AnswerNode
-    answer_nodes: list[AnswerNode] = field(default_factory=list)
+    parent: EvidenceNode
+    children: list[EvidenceNode] = field(default_factory=list)
 
 
-def stringify(root: AnswerNode) -> str:
+def stringify(root: EvidenceNode) -> str:
     lines = []
 
     def _build_string(
-        node: AnswerNode | QuestionGroup, prefix: str = "", is_last: bool = True
+        node: EvidenceNode | QuestionNode, prefix: str = "", is_last: bool = True
     ):
         connector = "└── " if is_last else "├── "
 
         match node:
-            case AnswerNode(answer, item_probabilities, _, children_groups):
+            case EvidenceNode(answer, item_probabilities, _, children_groups):
                 formatted_item_probabilities = ", ".join(
                     [f"{item}: {prob}" for item, prob in item_probabilities]
                 )
@@ -45,7 +45,7 @@ def stringify(root: AnswerNode) -> str:
                 for i, child in enumerate(children_groups):
                     new_prefix = prefix + ("    " if is_last else "│   ")
                     _build_string(child, new_prefix, i == len(children_groups) - 1)
-            case QuestionGroup(question, _, answer_nodes):
+            case QuestionNode(question, _, answer_nodes):
                 lines.append(f"{prefix}{connector}Question: {question}")
 
                 for i, child in enumerate(answer_nodes):
@@ -53,40 +53,40 @@ def stringify(root: AnswerNode) -> str:
                     _build_string(child, new_prefix, i == len(answer_nodes) - 1)
 
     formatted_item_probabilities = ", ".join(
-        [f"{item}: {prob}" for item, prob in root.item_probabilities]
+        [f"{item}: {prob}" for item, prob in root.belief_state]
     )
     lines.append(
         f"Answer: {root.answer} | Probabilities: [{formatted_item_probabilities}]"
     )
-    for i, child in enumerate(root.children_groups):
-        _build_string(child, "", i == len(root.children_groups) - 1)
+    for i, child in enumerate(root.children):
+        _build_string(child, "", i == len(root.children) - 1)
 
     return "\n".join(lines)
 
 
 if __name__ == "__main__":
-    root = AnswerNode(
-        "ROOT", item_probabilities=[("A", 0.25), ("B", 0.25), ("C", 0.25), ("D", 0.25)]
+    root = EvidenceNode(
+        "ROOT", belief_state=[("A", 0.25), ("B", 0.25), ("C", 0.25), ("D", 0.25)]
     )
 
-    question1 = QuestionGroup("Is it greater than or equal to B?", parent=root)
-    question1_affirmative = AnswerNode(
-        "Yes", item_probabilities=[("A", 0.5), ("B", 0.5)], this_group=question1
+    question1 = QuestionNode("Is it greater than or equal to B?", parent=root)
+    question1_affirmative = EvidenceNode(
+        "Yes", belief_state=[("A", 0.5), ("B", 0.5)], parent=question1
     )
-    question1_negative = AnswerNode(
-        "No", item_probabilities=[("C", 0.5), ("D", 0.5)], this_group=question1
+    question1_negative = EvidenceNode(
+        "No", belief_state=[("C", 0.5), ("D", 0.5)], parent=question1
     )
-    question1.answer_nodes.extend([question1_affirmative, question1_negative])
-    root.children_groups.append(question1)
+    question1.children.extend([question1_affirmative, question1_negative])
+    root.children.append(question1)
 
-    question2 = QuestionGroup("Is it an even letter?", parent=root)
-    question2_affirmative = AnswerNode(
-        "Yes", item_probabilities=[("A", 0.5), ("C", 0.5)], this_group=question2
+    question2 = QuestionNode("Is it an even letter?", parent=root)
+    question2_affirmative = EvidenceNode(
+        "Yes", belief_state=[("A", 0.5), ("C", 0.5)], parent=question2
     )
-    question2_negative = AnswerNode(
-        "No", item_probabilities=[("B", 0.5), ("D", 0.5)], this_group=question2
+    question2_negative = EvidenceNode(
+        "No", belief_state=[("B", 0.5), ("D", 0.5)], parent=question2
     )
-    question2.answer_nodes.extend([question2_affirmative, question2_negative])
-    root.children_groups.append(question2)
+    question2.children.extend([question2_affirmative, question2_negative])
+    root.children.append(question2)
 
     print(stringify(root))
