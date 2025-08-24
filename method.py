d854ed6bf689a953b417b5ee5a37bfc1dbbba8d7
DanielArnould <darn0005@student.monash.edu>
2025-08-24 20:11:45 +1000

    Create method

--- method.py
+++ method.py
@@ -8,55 +8,137 @@ getting and processing the examiner response and the guesser response
 - Tree state.
 """
 
-from chat_utils import ask_llm
-
-"""
-TODO: Create method functions to 
-orchestrate the tree creation and dialogue
-
-Also handle open sets and final logic.
-
-Also handle multiple benchmarks.
-"""
+from itertools import chain
+from node import EvidenceNode, QuestionNode
+from rewards import expected_future_reward
+from tasks.task import Task, InteractionMode
 
 
-def get_examiner_response(history: list[str]) -> str:
-    msg = [history[0]] + history[-3:] if len(history) > 3 else history
-    msg = "\n".join(msg)
-    return ask_llm(msg)
+def call_llm(val: str) -> str: ...
 
 
-def get_guesser_response(node: UoTNode) -> tuple[UoTNode, str]:
-    if node.is_terminal():
-        return node, node.items[0]
+class Method:
+    task: Task
+    max_lookahead_depth: int
+    max_conversation_depth: int
+    confidence_threshold: float
+
+    _current_node: EvidenceNode
+
+    def __init__(
+        self,
+        task: Task,
+        max_lookahead_depth: int,
+        max_conversation_depth: int,
+        confidence_threshold: float,
+    ):
+        self.task = task
+        self.max_lookahead_depth = max_lookahead_depth
+        self.max_conversation_depth = max_conversation_depth
+        self.confidence_threshold = confidence_threshold
+
+    def run(self) -> str:
+        initial_belief_state = self.task.get_initial_belief_state()
+        self._current_node = EvidenceNode(
+            answer="ROOT", belief_state=initial_belief_state, marginal_likelihood=1.0
+        )
+
+        while not self._is_terminal(self._current_node):
+            self._lookahead(self._current_node, 0)
+            best_question_node = max(
+                self._current_node.children, key=expected_future_reward
+            )
+
+            match self.task.interaction_mode:
+                case InteractionMode.INTERACTIVE:
+                    selected_evidence_node = self._pose_question(best_question_node)
+                case InteractionMode.BENCHMARK:
+                    selection_prompt = self.task.get_answer_selection_prompt(
+                        best_question_node
+                    )
+                    selection_output = call_llm(selection_prompt)
+                    selected_evidence_node = self.task.parse_answer_selection_output(
+                        selection_output, best_question_node
+                    )
+
+            self._current_node = selected_evidence_node
+
+        best_guess = max(
+            self._current_node.belief_state,
+            key=self._current_node.belief_state.__getitem__,
+        )
+        return best_guess
+
+    def _lookahead(self, node: EvidenceNode, curr_depth: int) -> None:
+        # Should we continue any further?
+        if curr_depth >= self.max_lookahead_depth or self._is_terminal(node):
+            return
+
+        # Have we already looked ahead at this stage?
+        if len(node.children) > 0:
+            for child in chain.from_iterable(
+                question.children for question in node.children
+            ):
+                self._lookahead(child, curr_depth + 1)
+
+            return
+
+        # Generate questions
+        question_gen_prompt = self.task.get_question_generation_prompt(node)
+        question_gen_output = call_llm(question_gen_prompt)
+        questions = self.task.parse_question_generation_output(question_gen_output)
+
+        for question in questions:
+            question_node = QuestionNode(question=question.question, parent=node)
+            likelihood_prompt = self.task.get_likelihood_elicitation_prompt(
+                node, question
+            )
+            likelihood_output = call_llm(likelihood_prompt)
+            likelihoods_for_all_answers = self.task.parse_likelihood_elicitation_output(
+                likelihood_output, question
+            )
+
+            for answer, likelihoods_for_answer in likelihoods_for_all_answers.items():
+                unnormalised_posterior = {
+                    hypo: node.belief_state[hypo] * likelihoods_for_answer[hypo]
+                    for hypo in node.belief_state
+                }
+
+                marginal_likelihood = sum(unnormalised_posterior.values())
+                posterior = {
+                    hypo: prob / marginal_likelihood
+                    for hypo, prob in unnormalised_posterior.items()
+                }
+                evidence_node = EvidenceNode(
+                    answer=answer,
+                    belief_state=posterior,
+                    marginal_likelihood=marginal_likelihood,
+                    parent=question_node,
+                )
+                question_node.children.append(evidence_node)
+                self._lookahead(evidence_node, curr_depth + 1)
+
+    def _pose_question(self, question_node: QuestionNode) -> EvidenceNode:
+        print("QUESTION".center(30, "="))
+        print(question_node.question)
+        print("ANSWERS".center(30, "="))
+        for i, evidence_node in enumerate(question_node.children, start=1):
+            print(f"{i}. {evidence_node.answer}")
+
+        user_selection = int(input("Select an answer: "))
+        return question_node.children[user_selection - 1]
+
+    def _get_conversation_depth(self, node: EvidenceNode) -> int:
+        if node.parent is None:
+            return 0
+
+        return 1 + self._get_conversation_depth(node.parent.parent)
+
+    def _is_terminal(self, node: EvidenceNode) -> bool:
+        return self._get_conversation_depth(node) >= self.max_conversation_depth or any(
+            prob >= self.confidence_threshold for prob in node.belief_state.values()
+        )
 
 
-def converse(target: str) -> None:
-    print(f"Target is {target}")
-    print("------ DIALOGUE START ------")
-
-    examiner_history = [prompts.examiner_prologue(target)]
-    guesser_history = [prompts.guesser_prologue()]
-
-    root = UoTNode()
-
-
-def mcts(node: UoTNode) -> tuple[str, UoTNode, UoTNode]: ...
-
-
-def get_user_response(question: str, history: list[str]) -> str: ...
-
-
-def algorithm():
-    history = []
-    time = 0
-    node = UoTNode()
-
-    while time < 20 and len(node.items) > 1:
-        question, yes_child, no_child = mcts(node)
-        answer = get_user_response(question, history)
-        node = yes_child if answer == "yes" else no_child
-        history.extend((question, answer))
-        time += 1
-
-    # check if answer is correct
+if __name__ == "__main__":
+    ...
