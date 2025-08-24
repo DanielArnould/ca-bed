9426e31786b4758513cabeab5cc13c618bb35da6
DanielArnould <darn0005@student.monash.edu>
2025-08-24 11:22:43 +1000

    Update belief state to dict

--- node.py
+++ node.py
@@ -6,14 +6,11 @@ may need to be differentiated and selected by the method.
 
 from dataclasses import dataclass, field
 
-type Probability = float
-type Item = str
-
 
 @dataclass
 class EvidenceNode:
     answer: str
-    belief_state: list[tuple[Item, Probability]]
+    belief_state: dict[str, float]
     parent: "QuestionNode | None" = None
     children: list["QuestionNode"] = field(default_factory=list)
 
@@ -34,30 +31,31 @@ def stringify(root: EvidenceNode) -> str:
         connector = "└── " if is_last else "├── "
 
         match node:
-            case EvidenceNode(answer, item_probabilities, _, children_groups):
-                formatted_item_probabilities = ", ".join(
-                    [f"{item}: {prob}" for item, prob in item_probabilities]
+            case EvidenceNode(answer, belief_state, _, children):
+                formatted_belief_state = ", ".join(
+                    [
+                        f"{hypothesis}: {prob}"
+                        for hypothesis, prob in belief_state.items()
+                    ]
                 )
                 lines.append(
-                    f"{prefix}{connector}Answer: {answer} | Probabilities: [{formatted_item_probabilities}]"
+                    f"{prefix}{connector}Answer: {answer} | Belief State: [{formatted_belief_state}]"
                 )
 
-                for i, child in enumerate(children_groups):
+                for i, child in enumerate(children):
                     new_prefix = prefix + ("    " if is_last else "│   ")
-                    _build_string(child, new_prefix, i == len(children_groups) - 1)
-            case QuestionNode(question, _, answer_nodes):
+                    _build_string(child, new_prefix, i == len(children) - 1)
+            case QuestionNode(question, _, children):
                 lines.append(f"{prefix}{connector}Question: {question}")
 
-                for i, child in enumerate(answer_nodes):
+                for i, child in enumerate(children):
                     new_prefix = prefix + ("    " if is_last else "│   ")
-                    _build_string(child, new_prefix, i == len(answer_nodes) - 1)
+                    _build_string(child, new_prefix, i == len(children) - 1)
 
-    formatted_item_probabilities = ", ".join(
-        [f"{item}: {prob}" for item, prob in root.belief_state]
+    formatted_belief_state = ", ".join(
+        [f"{hypothesis}: {prob}" for hypothesis, prob in root.belief_state.items()]
     )
-    lines.append(
-        f"Answer: {root.answer} | Probabilities: [{formatted_item_probabilities}]"
-    )
+    lines.append(f"Answer: {root.answer} | Belief State: [{formatted_belief_state}]")
     for i, child in enumerate(root.children):
         _build_string(child, "", i == len(root.children) - 1)
 
@@ -66,25 +64,25 @@ def stringify(root: EvidenceNode) -> str:
 
 if __name__ == "__main__":
     root = EvidenceNode(
-        "ROOT", belief_state=[("A", 0.25), ("B", 0.25), ("C", 0.25), ("D", 0.25)]
+        "ROOT", belief_state=dict([("A", 0.25), ("B", 0.25), ("C", 0.25), ("D", 0.25)])
     )
 
     question1 = QuestionNode("Is it greater than or equal to B?", parent=root)
     question1_affirmative = EvidenceNode(
-        "Yes", belief_state=[("A", 0.5), ("B", 0.5)], parent=question1
+        "Yes", belief_state=dict([("A", 0.5), ("B", 0.5)]), parent=question1
     )
     question1_negative = EvidenceNode(
-        "No", belief_state=[("C", 0.5), ("D", 0.5)], parent=question1
+        "No", belief_state=dict([("C", 0.5), ("D", 0.5)]), parent=question1
     )
     question1.children.extend([question1_affirmative, question1_negative])
     root.children.append(question1)
 
     question2 = QuestionNode("Is it an even letter?", parent=root)
     question2_affirmative = EvidenceNode(
-        "Yes", belief_state=[("A", 0.5), ("C", 0.5)], parent=question2
+        "Yes", belief_state=dict([("A", 0.5), ("C", 0.5)]), parent=question2
     )
     question2_negative = EvidenceNode(
-        "No", belief_state=[("B", 0.5), ("D", 0.5)], parent=question2
+        "No", belief_state=dict([("B", 0.5), ("D", 0.5)]), parent=question2
     )
     question2.children.extend([question2_affirmative, question2_negative])
     root.children.append(question2)
