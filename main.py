16334f10effedbd930d9298efe2c17aa9d5f66b0
DanielArnould <darn0005@student.monash.edu>
2025-08-25 23:27:09 +1000

    Add dummy main loop

--- main.py
+++ main.py
@@ -5,13 +5,24 @@ and API later.
 
 import logging
 
+from method import Method
+from models import Model
+from tasks.task import InteractionMode
+from tasks.twenty_questions.tasks import Bayesian
+
 
 def main():
     logging.basicConfig(
         level=logging.INFO,
         format="[%(asctime)s][%(levelname)s][%(name)s]: %(message)s",
     )
-    print("Oh :(. I'm not ready yet!")
+
+    model = Model.DUMMY
+    task = Bayesian(max_question_nodes=1, interaction_mode=InteractionMode.BENCHMARK)
+    method = Method(model, task, 3, 20, 0.8)
+    history = method.run()
+
+    # print(history)
 
 
 if __name__ == "__main__":
