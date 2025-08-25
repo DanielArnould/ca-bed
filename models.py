f0409484b488b86a863439b99ee483d1b80d72a9
DanielArnould <darn0005@student.monash.edu>
2025-08-25 22:23:59 +1000

    Add logging to models

--- models.py
+++ models.py
@@ -1,11 +1,13 @@
 from dataclasses import dataclass
 from enum import Enum, auto
+import logging
 from dotenv import load_dotenv
 import os
 
 from openai import OpenAI
 
 load_dotenv()
+LOGGER = logging.getLogger("LLM Models")
 
 
 class Model(Enum):
@@ -33,7 +35,10 @@ class LLMOutput:
 def _call_deepseek_chat(input_text: str) -> LLMOutput:
     assert DEEPSEEK_KEY is not None, "DEEPSEEK_KEY not found in environment!"
     client = OpenAI(api_key=DEEPSEEK_KEY, base_url="https://api.deepseek.com")
+    LOGGER.info("Connected to Deepseek Client")
 
+    LOGGER.info("Sending message to Deepseek Chat")
+    LOGGER.debug(f"Using prompt '{input_text}'")
     response = client.chat.completions.create(
         model="deepseek-chat",
         messages=[{"role": "user", "content": input_text}],
@@ -43,6 +48,8 @@ def _call_deepseek_chat(input_text: str) -> LLMOutput:
         stream=False,
     )
 
+    LOGGER.info("Received response from Deepseek Chat")
+    LOGGER.debug(f"Response received '{response}'")
     return LLMOutput(
         string=response.choices[0].message.content,  # type: ignore
         tokens=[
@@ -55,13 +62,18 @@ def _call_deepseek_chat(input_text: str) -> LLMOutput:
 def _call_deepseek_reasoner(input_text: str) -> LLMOutput:
     assert DEEPSEEK_KEY is not None, "DEEPSEEK_KEY not found in environment!"
     client = OpenAI(api_key=DEEPSEEK_KEY, base_url="https://api.deepseek.com")
+    LOGGER.info("Connected to Deepseek Client")
 
+    LOGGER.info("Sending message to Deepseek Reasoner")
+    LOGGER.debug(f"Using prompt '{input_text}'")
     response = client.chat.completions.create(
         model="deepseek-reasoner",
         messages=[{"role": "user", "content": input_text}],
         max_tokens=32_000,
     )
 
+    LOGGER.info("Received response from Deepseek Chat")
+    LOGGER.debug(f"Response received '{response}'")
     return LLMOutput(
         string=response.choices[0].message.content,  # type: ignore
         reasoning=response.choices[0].message.reasoning_content,  # type: ignore
