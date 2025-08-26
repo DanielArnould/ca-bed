51ca1727bd2fe7c76b2997b422ff1d25d4c24ddc
DanielArnould <darn0005@student.monash.edu>
2025-08-27 09:15:26 +1000

    Add test to main

--- main.py
+++ main.py
@@ -3,12 +3,16 @@ Unused for now, but will be an entry point for eval
 and API later.
 """
 
+import json
 import logging
+from pathlib import Path
 
 from method import Method
 from models import Model
 from tasks.task import InteractionMode
 from tasks.twenty_questions.tasks import Bayesian
+
+LOGGER = logging.getLogger("Main")
 
 
 def main():
@@ -18,11 +22,22 @@ def main():
     )
 
     model = Model.DUMMY
-    task = Bayesian(max_question_nodes=1, interaction_mode=InteractionMode.BENCHMARK)
-    method = Method(model, task, 3, 20, 0.8)
+    task = Bayesian(max_question_nodes=2, interaction_mode=InteractionMode.BENCHMARK)
+    method = Method(
+        model,
+        task,
+        max_lookahead_depth=2,
+        max_conversation_depth=3,
+        confidence_threshold=0.8,
+    )
     history = method.run()
+    output_path = Path("run.json")
 
-    # print(history)
+    LOGGER.info("Completed run, saving output to run.json...")
+    with output_path.open("w") as f:
+        json.dump(history, f)
+
+    LOGGER.info("Run saved to run.json")
 
 
 if __name__ == "__main__":
