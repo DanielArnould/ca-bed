c582c73fd443728b694723a5fe893d2d47a898b0
DanielArnould <darn0005@student.monash.edu>
2025-08-28 22:43:38 +1000

    Add basic eval functions

--- eval.py
+++ eval.py
@@ -1,16 +1,42 @@
-"""
-This module contains all necessary tools for evaluating a run.
-It should **ONLY** operate on saved records of runs and it
-should output the same metrics used by the original UoT paper:
-- Success rate
-- Mean Conversation Length
-- Mean Conversation Length in Succesful Cases
+from dataclasses import dataclass
 
-It should also create some helpful diagnostics such as:
-- Mean Depth
-- Max Depth
-- Node count
+from experiment_logging import RunHistory
 
-A reasonable first approach would be to first design a schema for how outputs and the tree should be saved
-so they can be parsed here again.
-"""
+
+@dataclass
+class RunEval:
+    success: bool
+    conversation_length: int
+
+
+@dataclass
+class GroupEval:
+    success_rate: float
+    mean_conversation_length: float
+    mean_conversation_length_in_successful_cases: float
+
+
+def get_run_eval(run_history: RunHistory) -> RunEval:
+    did_pass = (
+        run_history.actual_answer.strip().lower()
+        in run_history.final_answer.strip().lower()
+    )
+    conversation_length = len(run_history.final_path)
+    return RunEval(did_pass, conversation_length)
+
+
+def get_group_eval(run_evals: list[RunEval]) -> GroupEval:
+    success_rate = sum(run_eval.success for run_eval in run_evals)
+    mean_conversation_length = sum(
+        run_eval.conversation_length for run_eval in run_evals
+    ) / len(run_evals)
+
+    succesful_runs = [run_eval for run_eval in run_evals if run_eval.success]
+    mean_conversation_length_in_succesful_cases = sum(
+        run_eval.conversation_length for run_eval in succesful_runs
+    ) / len(succesful_runs)
+    return GroupEval(
+        success_rate,
+        mean_conversation_length,
+        mean_conversation_length_in_succesful_cases,
+    )
