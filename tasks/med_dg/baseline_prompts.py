ee203c324eb3a3e271af3854a0d11bb430d4e5d2
Rashad Aziz <rashadaziz.p@gmail.com>
2025-08-31 00:33:09 +0700

    Remove format restriction for targeting prompt

--- tasks/med_dg/baseline_prompts.py
+++ tasks/med_dg/baseline_prompts.py
@@ -107,5 +107,5 @@ def get_targeting_prompt() -> str:
     return dedent("""
         Note that you should point out and ask what disease the patient suffers from now.
         Refer to the past conversation regarding the patient's symptoms. Never repeat previously asked questions.
-        Ask the question with following the format: 'Are you experiencing [disease name]?'
+        The questions must start with 'Are you suffering from ...'
     """).strip()
