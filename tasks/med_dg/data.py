c29a56b1aae5a06d5f27503ab54fa719ed6baec9
DanielArnould <darn0005@student.monash.edu>
2025-08-31 13:13:36 +1000

    Parse MedDG dataset in more convenient way

--- tasks/med_dg/data.py
+++ tasks/med_dg/data.py
@@ -1,26 +1,38 @@
+from dataclasses import dataclass
 import json
 import os
+from pathlib import Path
 
 MED_DG_SET = [
-    'Enteritis', 
-    'Gastritis', 
-    'Gastroenteritis', 
-    'Esophagitis',
-    'Cholecystitis', 
-    'Appendicitis', 
-    'Pancreatitis', 
-    'Gastric ulcer',
-    'Constipation', 
-    'Cold', 
-    'Irritable bowel syndrome', 
-    'Diarrhea',
-    'Allergic rhinitis', 
-    'Upper respiratory tract infection',
-    'Pneumonia'
+    "Enteritis",
+    "Gastritis",
+    "Gastroenteritis",
+    "Esophagitis",
+    "Cholecystitis",
+    "Appendicitis",
+    "Pancreatitis",
+    "Gastric ulcer",
+    "Constipation",
+    "Cold",
+    "Irritable bowel syndrome",
+    "Diarrhea",
+    "Allergic rhinitis",
+    "Upper respiratory tract infection",
+    "Pneumonia",
 ]
 
-def load_data():
-    data_path = os.path.join(os.path.dirname(__file__), "MedDG.json")
-    with open(data_path, "r", encoding="utf-8") as f:
+
+@dataclass
+class MedDGInstance:
+    disease: str
+    self_report: str
+
+
+def load_data() -> list[MedDGInstance]:
+    data_path = Path(os.path.dirname(__file__), "MedDG.json")
+    with data_path.open("r", encoding="utf-8") as f:
         data = json.load(f)
-    return data
+    return [
+        MedDGInstance(disease=inst["target"], self_report=inst["self_repo"])
+        for inst in data
+    ]
