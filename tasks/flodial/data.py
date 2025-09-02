11dae6bdea7801e4ca21bd0af275dbe0425e62e1
Alex Kang <96352305+AlexK020908@users.noreply.github.com>
2025-09-02 10:06:43 +0800

    fixed flodial datasource

--- tasks/flodial/data.py
+++ tasks/flodial/data.py
@@ -6,8 +6,8 @@ from pathlib import Path
 
 @dataclass
 class FlodialInstance:
-    issue: str
-    cause: str
+    self_report: str
+    target: str
 
 
 def load_data() -> list[FlodialInstance]:
@@ -15,8 +15,8 @@ def load_data() -> list[FlodialInstance]:
     with data_path.open("r", encoding="utf-8") as f:
         data = json.load(f)
     return [
-        FlodialInstance(issue=issue, cause=cause)
-        for issue, cause in data.items()
+        FlodialInstance(self_report=instance["self_repo"], target=instance["target"])
+        for instance in data
     ]
 
 
@@ -24,12 +24,6 @@ def test():
     data = load_data()
     print(len(data))
     for instance in data:
-        print(f'issue: {instance.issue} - cause - {instance.cause}')
+        print(f'issue: {instance.self_report} - cause - {instance.target}')
         
-# test()
-
-
-def test2():
-    trouble_name = {"a":"b"}
-    print(trouble_name.values())
-test2()
+test()
