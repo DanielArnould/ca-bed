cf3920cd75b746a324526f3756e578a1c2ec35d3
DanielArnould <darn0005@student.monash.edu>
2025-09-06 12:13:39 +1000

    Update main for QC

--- main.py
+++ main.py
@@ -1,67 +1,66 @@
-"""
-Unused for now, but will be an entry point for eval
-and API later.
-"""
-
 import asyncio
 from datetime import datetime
 import json
-import multiprocessing
+import logging
 from pathlib import Path
 
 from experiment_logging import serialise_run_history
-from loggers import get_logger, setup_logger
-from method import Method
+from method import Method, get_uniform_belief_state
 from models import Model
-from tasks.task import Task
-from tasks.twenty_questions.data import COMMON
-from tasks.twenty_questions.tasks import Bayesian
+from question_clustering import QuestionClustering
+from tasks.med_dg.bayesian import Bayesian
+from tasks.med_dg.data import MED_DG_SET, load_data
 
-
-def run_task(task: Task, model: Model):
-    current_time_formatted = datetime.now().strftime("%Y%m%d_%H%M%S")
-    current_process = multiprocessing.current_process()
-    current_process.name = f"{task.__class__.__name__}-{task.task_answer}"
-    output_dir = Path(f"logs/{current_time_formatted}/")
-    output_dir.mkdir(parents=True, exist_ok=True)
-    setup_logger("Main", output_dir)
-    setup_logger("Method", output_dir)
-    setup_logger("LLM Models", output_dir)
-    logger = get_logger("Main")
-
-    method = Method(
-        model,
-        task,
-    )
-    history = asyncio.run(method.run())
-    run_history_path = output_dir / f"{current_process.name}_run.json"
-
-    logger.info(f"Completed run, saving output to {run_history_path}")
-    with run_history_path.open("w") as f:
-        json.dump(serialise_run_history(history), f)
-
-    logger.info(f"Run saved to {run_history_path}")
+LOGGER = logging.getLogger("Main")
 
 
 def main():
-    model = Model.DEEPSEEK_CHAT
+    benchmark_model = Model.DEEPSEEK_CHAT
+    method_model = Model.DEEPSEEK_CHAT
+    question_clustering = QuestionClustering()
+    initial_belief_state = get_uniform_belief_state(MED_DG_SET)
+
+    method = Method(
+        benchmark_model, method_model, question_clustering, initial_belief_state
+    )
+
+    dataset = load_data()
+    subset = dataset[:1]
     tasks = [
-        Baseline(
-            task_answer=item,
+        Bayesian(
+            task_answer=item.disease,
             max_question_nodes=3,
             max_lookahead_depth=3,
-            max_conversation_depth=20,
+            max_conversation_depth=5,
             confidence_threshold=0.7,
-            hypothesis_space=COMMON,
+            hypothesis_space=MED_DG_SET,
+            self_report=item.self_report,
         )
-        for item in COMMON[1:2]
+        for item in subset
     ]
 
-    num_cores = multiprocessing.cpu_count()
-    with multiprocessing.Pool(
-        processes=num_cores,
-    ) as pool:
-        pool.starmap(run_task, [(task, model) for task in tasks])
+    current_time_formatted = datetime.now().strftime("%Y%m%d_%H%M%S")
+    output_dir = Path(f"logs/{current_time_formatted}/")
+    output_dir.mkdir(parents=True, exist_ok=True)
+
+    for i, task in enumerate(tasks):
+        logging.basicConfig(
+            level=logging.INFO,
+            format="[%(asctime)s][%(levelname)s][%(name)s]: %(message)s",
+            handlers=[
+                logging.FileHandler(output_dir / f"{i}.log"),
+                logging.StreamHandler(),
+            ],
+            force=True,
+        )
+        history = asyncio.run(method.run(task))
+        run_history_path = output_dir / f"{i}.json"
+
+        LOGGER.info(f"Completed run, saving output to {run_history_path}")
+        with run_history_path.open("w") as f:
+            json.dump(serialise_run_history(history), f)
+
+        LOGGER.info(f"Run saved to {run_history_path}")
 
 
 if __name__ == "__main__":
