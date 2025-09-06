d10df9074a4dfc3d876f8e66592bfdbc60d66cf8
DanielArnould <darn0005@student.monash.edu>
2025-09-06 21:29:11 +1000

    Fix logging

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import json
 import logging
 from pathlib import Path
 
-from experiment_logging import serialise_run_history
+from history import serialise_run_history
 from method import Method, get_uniform_belief_state
 from models import Model
 from question_clustering import QuestionClustering
@@ -15,9 +15,23 @@ LOGGER = logging.getLogger("Main")
 
 
 def main():
+    output_dir = Path(f"logs/{datetime.now().strftime('%Y%m%d%H%M%S')}/")
+    output_dir.mkdir(parents=True, exist_ok=True)
+
+    logging.basicConfig(
+        level=logging.INFO,
+        format="[%(asctime)s][%(levelname)s][%(name)s]: %(message)s",
+        handlers=[
+            logging.StreamHandler(),
+            logging.FileHandler(output_dir / "setup.log"),
+        ],
+        force=True,
+    )
+
     benchmark_model = Model.DEEPSEEK_CHAT
     method_model = Model.DEEPSEEK_CHAT
-    question_clustering = QuestionClustering()
+    LOGGER.info(f"Benchmarker: {benchmark_model.name} Method: {method_model.name}")
+    question_clustering = QuestionClustering(threshold=0.95)
     initial_belief_state = get_uniform_belief_state(MED_DG_SET)
 
     method = Method(
@@ -25,23 +39,19 @@ def main():
     )
 
     dataset = load_data()
-    subset = dataset[:1]
+    subset = dataset[:50]
     tasks = [
         Bayesian(
             task_answer=item.disease,
             max_question_nodes=3,
             max_lookahead_depth=3,
             max_conversation_depth=5,
-            confidence_threshold=0.7,
             hypothesis_space=MED_DG_SET,
+            confidence_threshold=0.7,
             self_report=item.self_report,
         )
         for item in subset
     ]
-
-    current_time_formatted = datetime.now().strftime("%Y%m%d_%H%M%S")
-    output_dir = Path(f"logs/{current_time_formatted}/")
-    output_dir.mkdir(parents=True, exist_ok=True)
 
     for i, task in enumerate(tasks):
         logging.basicConfig(
