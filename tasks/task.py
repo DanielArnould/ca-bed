1899fdb87b4e25e54b346a5c0845aef92dcea557
DanielArnould <darn0005@student.monash.edu>
2025-09-24 20:48:34 +1000

    Remove unused abstract methods

--- tasks/task.py
+++ tasks/task.py
@@ -40,14 +40,6 @@ class Task(ABC):
         pass
 
     @abstractmethod
-    def get_prior_prompt(self) -> str:
-        pass
-
-    @abstractmethod
-    def parse_prior_output(self, output: str) -> dict[str, float]:
-        pass
-
-    @abstractmethod
     def get_question_generation_prompt(self, current_node: EvidenceNode) -> str:
         """
         From the current node, create a prompt that asks for new questions
